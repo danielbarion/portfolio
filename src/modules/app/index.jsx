d1fcfc85d680d60012d1a1bc9758b3b7d84185bc
Daniel Barion <danielbarionn@gmail.com>
2019-05-28 00:51:46 -0300

    Start navbar

--- src/modules/app/index.jsx
+++ src/modules/app/index.jsx
@@ -32,18 +32,31 @@ class App extends Component {
 		 */
 		const _root = 'app'
 		const _header = `${_root}-header`
+		const _navigation = `${_header}-navigation`
 
 		/**
 		 * render functions
 		 */
-		const main = (context) => (
+		const main = () => (
 			<div className={_root}>
 				{header()}
 			</div>
 		)
 
 		const header = () => (
-			<div className={_header}>
+			<div className={_header} id="header">
+				{navigation()}
+			</div>
+		)
+
+		const navigation = () => (
+			<div className={_navigation}>
+				<ul>
+					<li><a href="#header">Home</a></li>
+					<li><a href="#header">About</a></li>
+					<li><a href="#header">Projects</a></li>
+					<li><a href="#header">Contact</a></li>
+				</ul>
 			</div>
 		)
 
